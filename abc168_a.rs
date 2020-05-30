65f0f6500b83dfd62de60a0b4092920d70e79ee3
Masahiro H <mhg00g13@gmail.com>
2020-05-30 13:07:15 +0900

    Update abc168_a.rs

--- abc168_a.rs
+++ abc168_a.rs
@@ -1,11 +1,10 @@
-fn main()
-{
+fn main() {
     let n: i32 = {
         let mut s = String::new();
         std::io::stdin().read_line(&mut s).unwrap();
         s.trim_end().to_owned().parse().unwrap()
     };
-    
+
     match n % 10 {
         2 | 4 | 5 | 7 | 9 => println!("hon"),
         0 | 1 | 6 | 8 => println!("pon"),
