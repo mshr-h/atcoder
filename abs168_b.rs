8631edf4bb5eb5596107183fea3a18f38c9475d3
Masahiro H <mhg00g13@gmail.com>
2020-05-30 13:06:43 +0900

    Update abs168_b.rs

--- abs168_b.rs
+++ abs168_b.rs
@@ -4,21 +4,21 @@ fn main() {
         std::io::stdin().read_line(&mut s).unwrap();
         s.trim_end().to_owned().parse().unwrap()
     };
- 
+
     let s = {
         let mut s = String::new();
         std::io::stdin().read_line(&mut s).unwrap();
         s.trim_end().to_owned()
     };
- 
+
     if s.chars().count() > k {
-        let mut str1 = "".to_string();
+        let mut substr = "".to_string();
         for (i, c) in s.chars().enumerate() {
             if i < k {
-                str1.push(c);
+                substr.push(c);
             }
         }
-        println!("{}...", str1);
+        println!("{}...", substr);
     } else {
         println!("{}", s);
     }
